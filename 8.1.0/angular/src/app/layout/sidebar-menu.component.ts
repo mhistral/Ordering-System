eb3a8ff89188917dba7840396c27fec9f1250f57
Kimberly Grace Parrenas <Kimberly.Parrenas@dbaadvisory.com>
2023-08-18 11:56:40 +0800

    sidebar update

--- 8.1.0/angular/src/app/layout/sidebar-menu.component.ts
+++ 8.1.0/angular/src/app/layout/sidebar-menu.component.ts
@@ -57,64 +57,79 @@ export class SidebarMenuComponent extends AppComponentBase implements OnInit {
                 'fas fa-building',
                 'Pages.Tenants'
             ),
-            new MenuItem(
-                this.l('Users'),
-                '/app/users',
-                'fas fa-users',
-                'Pages.Users'
-            ),
-            new MenuItem(this.l('MultiLevelMenu'), '', 'fas fa-circle', '', [
-                new MenuItem('ASP.NET Boilerplate', '', 'fas fa-dot-circle', '', [
-                    new MenuItem(
-                        'Home',
-                        'https://aspnetboilerplate.com?ref=abptmpl',
-                        'far fa-circle'
-                    ),
-                    new MenuItem(
-                        'Templates',
-                        'https://aspnetboilerplate.com/Templates?ref=abptmpl',
-                        'far fa-circle'
-                    ),
-                    new MenuItem(
-                        'Samples',
-                        'https://aspnetboilerplate.com/Samples?ref=abptmpl',
-                        'far fa-circle'
-                    ),
-                    new MenuItem(
-                        'Documents',
-                        'https://aspnetboilerplate.com/Pages/Documents?ref=abptmpl',
-                        'far fa-circle'
-                    ),
-                ]),
-                new MenuItem('ASP.NET Zero', '', 'fas fa-dot-circle', '', [
-                    new MenuItem(
-                        'Home',
-                        'https://aspnetzero.com?ref=abptmpl',
-                        'far fa-circle'
-                    ),
-                    new MenuItem(
-                        'Features',
-                        'https://aspnetzero.com/Features?ref=abptmpl',
-                        'far fa-circle'
-                    ),
-                    new MenuItem(
-                        'Pricing',
-                        'https://aspnetzero.com/Pricing?ref=abptmpl#pricing',
-                        'far fa-circle'
-                    ),
-                    new MenuItem(
-                        'Faq',
-                        'https://aspnetzero.com/Faq?ref=abptmpl',
-                        'far fa-circle'
-                    ),
-                    new MenuItem(
-                        'Documents',
-                        'https://aspnetzero.com/Documents?ref=abptmpl',
-                        'far fa-circle'
-                    )
-                ])
+            
+            new MenuItem(this.l('User Masterlist'), '', 'fa-solid fa-address-book', '', [
+               new MenuItem(
+                  this.l('Users'),
+                  '/app/users',
+                  'fas fa-users',
+                  'Pages.Users'
+              ),
+              new MenuItem(
+                  this.l('Customers'),
+                  '/app/users',
+                  'fa-solid fa-user-group',
+                  'Pages.Users'
+           ),
+               new MenuItem(
+                  this.l('Vendors'),
+                  '/app/users',
+                  'fa-solid fa-shop',
+                  'Pages.Users'
+               ),
+            ]),
+         new MenuItem(this.l('MultiLevelMenu'), '', 'fas fa-circle', '', [
+            new MenuItem('ASP.NET Boilerplate', '', 'fas fa-dot-circle', '', [
+               new MenuItem(
+                  'Home',
+                  'https://aspnetboilerplate.com?ref=abptmpl',
+                  'far fa-circle'
+               ),
+               new MenuItem(
+                  'Templates',
+                  'https://aspnetboilerplate.com/Templates?ref=abptmpl',
+                  'far fa-circle'
+               ),
+               new MenuItem(
+                  'Samples',
+                  'https://aspnetboilerplate.com/Samples?ref=abptmpl',
+                  'far fa-circle'
+               ),
+               new MenuItem(
+                  'Documents',
+                  'https://aspnetboilerplate.com/Pages/Documents?ref=abptmpl',
+                  'far fa-circle'
+               ),
+            ]),
+            new MenuItem('ASP.NET Zero', '', 'fas fa-dot-circle', '', [
+               new MenuItem(
+                  'Home',
+                  'https://aspnetzero.com?ref=abptmpl',
+                  'far fa-circle'
+               ),
+               new MenuItem(
+                  'Features',
+                  'https://aspnetzero.com/Features?ref=abptmpl',
+                  'far fa-circle'
+               ),
+               new MenuItem(
+                  'Pricing',
+                  'https://aspnetzero.com/Pricing?ref=abptmpl#pricing',
+                  'far fa-circle'
+               ),
+               new MenuItem(
+                  'Faq',
+                  'https://aspnetzero.com/Faq?ref=abptmpl',
+                  'far fa-circle'
+               ),
+               new MenuItem(
+                  'Documents',
+                  'https://aspnetzero.com/Documents?ref=abptmpl',
+                  'far fa-circle'
+               )
             ])
-        ];
+         ])
+      ];
     }
 
     patchMenuItems(items: MenuItem[], parentId?: number): void {
